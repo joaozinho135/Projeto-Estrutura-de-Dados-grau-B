eb9acdcf925955ee46f5eedfc1d7d79bdccd64aa
João Pedro Tisatto Salvador <63618519+joaozinho135@users.noreply.github.com>
2022-11-26 10:52:22 -0300

    Update Trabalho Grau B - Caminho Minimo.cpp
    Agora a matriz está aparecendo como resultado.

--- Trabalho Grau B - Caminho Minimo.cpp
+++ Trabalho Grau B - Caminho Minimo.cpp
@@ -1,30 +1,33 @@
 #include<stdio.h>
 #include<stdlib.h>
-
+#define Lin 10
+#define Col 10
 int main()
 {
-	int numeros=0;
+	int l, c, Mat[Lin][Col]; 
+    char estado[30];
 	
 	FILE *arq_matriz;
-	arquivo = fread("Matriz.txt", "w");
+	arq_matriz = fopen("Matriz.txt", "r");
 	
 	FILE *arq_eixos;
-	arquivo = fread("Eixos.txt", "w");
+	arq_eixos = fopen("Eixos.txt", "r");
 	
 	if (arq_matriz == NULL or arq_eixos == NULL)
 	{
-		printf("Matriz ou Eixos est�o vazios!");
+		printf("Matriz ou Eixos estão vazios!");
 	}
 	else {
-		printf("Arquivo criado com sucesso!\n");
-		
-		for (numeros = 0; numeros <=40; numeros++)
-		{
-			if(numeros % 2 == 0)
-			{
-				fprintf(arquivo, "%d", numeros);
-			}
-		}
-		fclose(arquivo);
+	
+    for(l=0;l<Lin;l++){
+        for(c=0;c<Col;c++){
+            fscanf(arq_matriz,"%d ", &Mat[l][c]);
+            fgets(estado, 30, arq_eixos);    
+            printf("%d ", Mat[l][c]);//testar se a impressão esta correta
+    }
+    printf("\n");
+    }
+    fclose(arq_matriz); //fechar arquivo
+    return 0;
 	}
 }
